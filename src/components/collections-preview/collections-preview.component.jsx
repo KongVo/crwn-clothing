99828a592a1abf401fa829d5ed047e92f86e7925
Kong Vo <>
2021-05-03 20:43:58 -0700

    update reducer, selector, redux flow

--- src/components/collections-preview/collections-preview.component.jsx
+++ src/components/collections-preview/collections-preview.component.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 import CollectionItem from '../collection-item/collection-item.component';
 
-import './collection-preview.styles.css';
+import './collections-preview.styles.css';
 
 const CollectionPreview = ({title, items}) => (
     <div className='collection-preview'>
